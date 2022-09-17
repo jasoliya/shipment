9b0076e08b62e9296340bfcee37c919143971988
Jasoliya Brijesh <jasoliyabrijesh123@gmail.com>
2022-09-17 16:41:56 +0530

    changes

--- middleware/api.js
+++ middleware/api.js
@@ -4,6 +4,10 @@ import { Checkout } from '@shopify/shopify-api/dist/rest-resources/2022-07/index
 export function apiEndPoints(app) {
     app.post('/bundle/add', async (req, res) => {
         const session = await Shopify.Utils.loadCurrentSession(req, res, false);
+        if(!session) {
+            res.status(401).send('Could not find session');
+            return;
+        }
         res.status(200).send({success: session});
     });
 }
