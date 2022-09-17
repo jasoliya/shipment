41dfd98a3b95afdcebd8c12a5ca349bc67e6188d
Jasoliya Brijesh <jasoliyabrijesh123@gmail.com>
2022-09-17 16:38:01 +0530

    changes

--- middleware/api.js
+++ middleware/api.js
@@ -1,5 +1,9 @@
+import { Shopify } from '@shopify/shopify-api';
+import { Checkout } from '@shopify/shopify-api/dist/rest-resources/2022-07/index.js';
+
 export function apiEndPoints(app) {
     app.post('/bundle/add', async (req, res) => {
-        res.status(200).send({success: req.body});
+        const session = await Shopify.Utils.loadCurrentSession(req, res, false);
+        res.status(200).send({success: session});
     });
 }
