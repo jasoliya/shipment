634f08c8c26eeff156daa6044c8beafdf1455f3e
Jasoliya Brijesh <jasoliyabrijesh123@gmail.com>
2022-09-19 12:25:40 +0530

    changes

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import { Shopify, LATEST_API_VERSION } from '@shopify/shopify-api';
-import { apiEndPoints } from './middleware/api.js';
+//import dotenv from 'dotenv';
+//import { Shopify, LATEST_API_VERSION } from '@shopify/shopify-api';
+//import { apiEndPoints } from './middleware/api.js';
 
 import cors from 'cors';
 
-dotenv.config();
+//dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || `http://localhost:${PORT}/`;
+// const HOST = process.env.HOST || `http://localhost:${PORT}/`;
 
-Shopify.Context.initialize({
-    API_KEY: process.env.SHOPIFY_API_KEY,
-    API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
-    SCOPES: process.env.SCOPES.split(','),
-    HOST_NAME: HOST.replace(/https?:\/\//,''),
-    HOST_SCHEME: HOST.split('://')[0],
-    API_VERSION: LATEST_API_VERSION
-});
+// Shopify.Context.initialize({
+//     API_KEY: process.env.SHOPIFY_API_KEY,
+//     API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
+//     SCOPES: process.env.SCOPES.split(','),
+//     HOST_NAME: HOST.replace(/https?:\/\//,''),
+//     HOST_SCHEME: HOST.split('://')[0],
+//     API_VERSION: LATEST_API_VERSION
+// });
 
-apiEndPoints(app);
+// apiEndPoints(app);
 
 const isEmpty = (obj) => Object.keys(obj).length === 0;
 
