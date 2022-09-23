06cd5dea09c4f308de741a6807322c368949bb20
Jasoliya Brijesh <jasoliyabrijesh123@gmail.com>
2022-09-23 16:26:24 +0530

    changes

--- index.js
+++ index.js
@@ -16,14 +16,19 @@ const isEmpty = (obj) => Object.keys(obj).length === 0;
 app.post('/order/get', async (req, res) => {
     const order = req.body;
 
-    if(isEmpty(order)) {
-        res.status(401).send('Cannot get order data');
-        return;
-    }
-    
-    console.log(typeof order.tags,order.tags);
+    if(isEmpty(order)) return res.status(401).send('Cannot get order data');
+    if(!order.tags) return res.status(401).send('Declined');
+    const tags = order.tags.toLowerCase();
+    const apiToken = null;
+    if(tags.indexOf('zdravko') >= 0) apiToken = 'SFMyNTY.g2gDYgAAAVxuBgBqUnRUgwFiAAFRgA.4y6kc1PZMhv1oTb0mt4Wxm0QN4ZCO9IWaszBaiT_74Y';
+    if(tags.indexOf('muppet') >= 0) apiToken = 'SFMyNTY.g2gDYgAABkRuBgC7eelpgwFiAAFRgA.95slglj5zg_kxToaK6EiCfxA_dodx7LRcUoHFuI_P1A';
+    if(!apiToken) return res.status(401).send('Shipment not found');
 
-    let postData = {},shipping_payment_method = 'И-Г';
+    let postData = {}, shipping_payment_method;
+    const shippingAmount = parseInt(order.total_shipping_price_set.shop_money.amount);
+    
+    if(tags.indexOf('zdravko') >= 0) shipping_payment_method = shippingAmount > 0 ? 'П-Г' : 'И-Г';
+    if(tags.indexOf('muppet') >= 0)  shipping_payment_method = shippingAmount > 0 ? 'П-Ф' : 'И-Ф';
 
     postData.receiver = {
         "name": order.shipping_address.name,
@@ -34,7 +39,7 @@ app.post('/order/get', async (req, res) => {
     postData.package_value = order.subtotal_price;
     postData.number_packages = 1;
     postData.shipping_payment_method = shipping_payment_method;
-    postData.shipment_cost = order.total_shipping_price_set.shop_money.amount;
+    postData.shipment_cost = "130";
     postData.order_number = order.name;
 
     if(order.note) postData.note = order.note || 'Note';
@@ -45,7 +50,7 @@ app.post('/order/get', async (req, res) => {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'SFMyNTY.g2gDYgAAAVxuBgBqUnRUgwFiAAFRgA.4y6kc1PZMhv1oTb0mt4Wxm0QN4ZCO9IWaszBaiT_74Y'
+            'Authorization': apiToken
         }
     }
     const httpReq = https.request(opt, httpRes => {
